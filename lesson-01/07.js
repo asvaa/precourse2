ad02171a7bab1011fb25104f6e2d6dc4ef5535ae
Адам Мусаев <adam.mysaev@bk.ru>
2023-12-14 13:49:11 +0300

    foxs

---
+++ lesson-01/07.js
@@ -0,0 +1,5 @@
+//Выведи в консоль «Я красавчик!».
+// Обрати внимание: «Я» с заглавной.
+let iAm = "я";
+let krasavchik = "красавчик!";
+console.log(iAm.charAt(0).toUpperCase() + iAm.slice(1) + " " + krasavchik);
