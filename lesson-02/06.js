65b352a342736031a6fcfa7982ece022c8be2d0a
Адам Мусаев <adam.mysaev@bk.ru>
2023-12-15 15:01:02 +0300

    bob

--- lesson-02/06.js
+++ lesson-02/06.js
@@ -3,20 +3,20 @@
 //(Т.е. необходимо создать глубокую копию объекта passportWithAddress, а затем 'LA' заменить на 'Bobryisk' в city).
 
 let passportWithAddress = {
-  name: "Petr",
-  surname: "Petrov",
-  address: {
-      country: "USA",
-      city: "LA"
-  }
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "LA"
+    }
 };
 
 // Создаем глубокую копию объекта passportWithAddress
-let copiedPassport = JSON.parse(JSON.stringify(passportWithAddress));
+let copiedObject = JSON.parse(JSON.stringify(passportWithAddress));
 
 // Меняем значение 'LA' на 'Bobryisk' в скопированном объекте
 copiedPassport.address.city = 'Bobryisk';
 
 // Выводим оригинальный объект и его копию для проверки
-console.log("Оригинальный паспорт:", passportWithAddress);
-console.log("Копия паспорта с изменениями:", copiedPassport);
+console.log("Оригинальный объект:", passportWithAddress);
+console.log("Копия объекта с изменениями:", copiedObject);
