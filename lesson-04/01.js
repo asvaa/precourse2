9fc8451a855e7929d69c8a7872ac0ff460b8d1ae
Адам Мусаев <adam.mysaev@bk.ru>
2023-12-27 10:47:11 +0300

    nyti

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -1,1 +1,29 @@
-// code
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+const clockElement = document.getElementById("clock");
+
+let intervalId = null;
+
+function startClock() {
+  if (!intervalId) {
+    intervalId = setInterval(updateClock, 1000);
+  }
+}
+
+function stopClock() {
+  if (intervalId) {
+    clearInterval(intervalId);
+    intervalId = null;
+  }
+}
+
+function updateClock() {
+  const now = new Date();
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  const seconds = String(now.getSeconds()).padStart(2, "0");
+  clockElement.textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+startButton.addEventListener("click", startClock);
+stopButton.addEventListener("click", stopClock);
